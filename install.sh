9dd9c8790a0a9a080bd6c23d40fd7fc1958fc38d
Jason Schneider <jason.phillip.schneider@gmail.com>
2019-07-13 02:02:19 -0500

    kj
    i

--- install.sh
+++ install.sh
@@ -12,12 +12,12 @@ PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ansible|grep "install ok insta
 echo "Checking for Ansible: $PKG_OK"
 
 if [[ "" == "$PKG_OK" ]]; then
-    sudo apt-get update
-    sudo apt-get install -q -y software-properties-common
-    sudo add-apt-repository -y ppa:ansible/ansible
+    apt-get update
+         apt-get install -q -y software-properties-common
+         add-apt-repository -y ppa:ansible/ansible
 
-    sudo apt-get update
-    sudo apt-get install -q -y python2.7 python-pip python-mysqldb ansible
+         apt-get update
+         apt-get install -q -y python2.7 python-pip python-mysqldb ansible
 fi
 
 APP_ENV="${APP_ENV:-production}"
